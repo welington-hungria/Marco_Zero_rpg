bf1712fe0d827c6f51fa06b9a15347cc4a8b07b6
Welington Hungria <welingtonph@gmail.com>
2024-02-18 21:01:00 -0300

    init

--- Caracter/scripts/character.gd
+++ Caracter/scripts/character.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
+
+var _state_machine
+
 @export_category("Variables")
 @export var _move_speed: float = 64.0
 @export var _friction: float = 0.2
 @export var _acceleration: float = 0.2
 
+@export_category("Objects")
+@export var _animatrion_tree: AnimationTree = null
+
+func _ready() -> void:
+	_state_machine = _animatrion_tree["parameters/playback"]
+
 func _physics_process(_delta: float) -> void:
 	_move()
+	_animate()
 	move_and_slide()
 		
 func _move() -> void:
@@ -15,6 +25,9 @@ func _move() -> void:
 	)
 	
 	if _direction != Vector2.ZERO:
+		_animatrion_tree["parameters/idle/blend_position"] = _direction
+		_animatrion_tree["parameters/walk/blend_position"] = _direction
+		
 		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
 		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
 		return
@@ -22,4 +35,9 @@ func _move() -> void:
 	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
 	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
 	
-	
+func _animate() -> void:
+	if velocity.length() > 2:
+		_state_machine.travel("walk")
+		return
+		
+	_state_machine.travel("idle")
